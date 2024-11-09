56090f29c315b6f15b16d867adc5d128e4e479bb
Keerthana <146184207+bkeerthanapgpcet@users.noreply.github.com>
2024-11-09 15:39:52 +0530

    update

---
+++ src/components/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Contact = () => (
+  <section id="contact">
+    <h2>Contact</h2>
+    <p><b>Email:</b> bkeerthanapgpcet@gmail.com</p>
+    <p><b>LinkedIn:</b> https://www.linkedin.com/in/keerthana-bala-624538333/
+    </p>
+    <a className='top' href='#head'>Back to top</a>
+  </section>
+);
+
+export default Contact;
